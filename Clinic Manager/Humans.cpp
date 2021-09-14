cf734d2c1e9aefc51c5824b02dab05dcf300e41e
Antonyesk601 <40663824+Antonyesk601@users.noreply.github.com>
2021-09-14 22:00:40 +0200

    PetOwners Finished (iteration 1)

--- Clinic Manager/Humans.cpp
+++ Clinic Manager/Humans.cpp
@@ -39,7 +39,7 @@ Human::Human(const Human& human)
 	}
 }
 
-Human::Human(std::string name, std::string number, std::string  contact, bool useDetail, FinancialRecord record)
+Human::Human(const std::string& name, const std::string& number,const std::string&  contact, bool useDetail,const FinancialRecord& record)
 {
 	SetName(name);
 	phoneNumbers = new std::list<std::string>();
@@ -106,6 +106,28 @@ void Human::DelFiscalInfoDetail(const FinancialRecord& record)
 	delete& record;
 }
 
+const std::string& Human::GetName()
+{
+	return name;
+}
+
+const std::list<std::string>* Human::GetPhone()
+{
+	return phoneNumbers;
+}
+
+const std::list<std::string>* Human::GetOnlineContacts()
+{
+	return onlineContacts;
+}
+
+const std::list<FinancialRecord>* Human::getFinancialDetails()
+{
+	return fiscalInfoDetails;
+}
+
+
+
 FinancialRecord::FinancialRecord() 
 {
 	fiscalinfo = 0.00f;
@@ -116,4 +138,62 @@ FinancialRecord::FinancialRecord(float value, bool Fulfilled)
 {
 	fiscalinfo = value;
 	fulfilled = Fulfilled;
+}
+
+
+
+PetOwner::PetOwner(const std::string& name, const std::string& number, const std::string&  contact,const bool& useDetail,const FinancialRecord& record, Pet& pet):Human(name,number,contact,useDetail,record)
+{
+	AddPet(pet);
+	CoOwners = new std::list<PetOwner&>();
+}
+
+PetOwner::PetOwner() : Human() 
+{
+	Pets = new std::list<Pet&>();
+	Pet pet=Pet();
+	AddPet(pet);
+	CoOwners = new std::list<PetOwner&>();
+}
+
+PetOwner::PetOwner(const PetOwner& Other):Human(Other)
+{
+	for(Pet p:*(Other.Pets))
+		AddPet(p);
+	for(PetOwner co:*(Other.CoOwners))
+		AddCoOwner(co);
+}
+
+void PetOwner::AddCoOwner(PetOwner& other) 
+{
+	other.CoOwners->push_back(*this);
+	this->CoOwners->push_back(other);
+	other.Pets = this->Pets;
+}
+
+void PetOwner::DelCoOwner(PetOwner& other) 
+{
+	other.CoOwners->remove(*this);
+	this->CoOwners->remove(other);
+	other.Pets = new std::list<Pet&>();
+}
+
+const std::list<PetOwner&>* PetOwner::getCoOwners() 
+{
+	return CoOwners;
+}
+
+void PetOwner::AddPet(Pet& newPet)
+{
+	Pets->push_back(newPet);
+}
+
+void PetOwner::DelPet(Pet& removedPet)
+{
+	Pets->remove(removedPet);
+}
+
+const std::list<Pet&>* PetOwner::getPets()
+{
+	return Pets;
 }
--- Clinic Manager/Humans.h
+++ Clinic Manager/Humans.h
@@ -15,7 +15,7 @@ class Human
 public:
 	Human();
 	Human(const Human&);
-	Human(std::string, std::string = "00000000000", std::string = "facebook.com",bool=true, FinancialRecord = FinancialRecord());
+	Human(const std::string&,const std::string& = "00000000000",const std::string& = "facebook.com", bool=true,const FinancialRecord& = FinancialRecord());
 public:
 	void SetName(const std::string&);
 	void SetPhone(const std::string&);
@@ -24,7 +24,13 @@ public:
 	void DelOnlineContact(const std::string&);
 	void SetFiscalInfoDetail (const FinancialRecord&);
 	void DelFiscalInfoDetail (const FinancialRecord&);
-
+	const std::string& GetName();
+	const std::list<std::string>*  GetPhone();
+	const std::list<std::string>* GetOnlineContacts();
+	const std::list<FinancialRecord>* getFinancialDetails();
+	void DelOnlineContact(const std::string&);
+	void SetFiscalInfoDetail(const FinancialRecord&);
+	void DelFiscalInfoDetail(const FinancialRecord&);
 private:
 	void CalculateFiscalInfoSum();
 private:
@@ -42,11 +48,16 @@ class PetOwner : public Human
 public:
 	PetOwner();
 	PetOwner(const PetOwner&);
-	PetOwner(const std::string&,const std::string&,const std::string&,const bool&, const FinancialRecord&,const std::list<Pet>&);
+	PetOwner(const std::string&,const std::string&,const std::string&,const bool&, const FinancialRecord&, Pet&);
 public:
-	void MakeCoOwner(const PetOwner&);
-private:
+	void AddCoOwner(PetOwner&);
+	void DelCoOwner(PetOwner&);
+	const std::list<PetOwner&>* getCoOwners();
+	void AddPet(Pet&);
+	void DelPet(Pet&);
+	const std::list<Pet&>* getPets();
 
 private:
-	
+	std::list<Pet&>* Pets;
+	std::list<PetOwner&>* CoOwners;
 };
